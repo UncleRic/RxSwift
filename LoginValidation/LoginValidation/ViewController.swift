29b3d45a77cb9b07712452d5944049e4550d0adf
Frederick C. Lee <frederick.c.lee@gmail.com>
2017-10-04 15:07:50 -0700

    Initial Stub

---
+++ LoginValidation/LoginValidation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginValidation
+//
+//  Created by Frederick C. Lee on 10/4/17.
+//  Copyright © 2017 Amourine Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
